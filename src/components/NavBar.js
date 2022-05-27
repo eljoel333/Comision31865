b2c53c0ffb141c3de6dd9eae2268894ae5749a4f
eljoel333 <joel.gasparhernandez@gmail.com>
2022-05-27 17:26:51 -0500

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,18 @@
-import React, {Component} from 'react';
+
 import OptionsMenu from '../container/ItemListContainer'; 
 
 
 export default function NavBar(){
-Component.state = {clicked:false};
 
-const handleClick = ()=>{
-    this.setState({clicked: !this.state.clicked})
-}
     return (
         <nav className='NavbarItems'> 
                <h1 className='navbar-logo'>React <i className='fab fa-react'></i>
 
                </h1>
-               <div className='menu-icon' onClick={handleClick}>
-                   <i className={this.state.clicked?'':''}></i>
+               <div className='menu-icon' >
+                   <i className='fas fa-bar'></i>
                </div>
-               <ul>
+               <ul className='nav-menu'>
                    {OptionsMenu.map((item,index)=>{
                        return (
                         <li key={index}> <a className={item.className} href={item.url}> {item.titulo}</a> </li>
