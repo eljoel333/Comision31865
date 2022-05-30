3487d2eda3502acd9d598c26cc58115261abe1bc
eljoel333 <joel.gasparhernandez@gmail.com>
2022-05-30 17:31:29 -0500

    counter

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Items from './components/Items';
-
-
-//importamos las pages
-
-import Page1 from './container/Page1';
-import Page2 from './container/Page2';
-import Page3 from './container/Page3';
-
+import ItemList from './components/ItemList/ItemList';
+import Counter from './components/Counter/Counter';
 function App() {
   return (
     <div className='container'>
       <h1>Mi logo</h1>
        <NavBar></NavBar>
       <Items nombre='Mis Productos'></Items>
+      <item></item>
+     <Counter initial={1} stock={12} title='hola'></Counter>
 
     </div>
   );
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+const Counter = ({initial, stock, title}) =>{
+    const [count, setCount] = useState(initial);
+
+    const decrement = () =>{
+        setCount(prev => prev - 1)
+    }
+
+    const increment =() =>{
+        count < stock && setCount(count + 1); 
+    }
+
+    const reset = () =>{
+        setCount(initial)
+    }
+
+
+    return (
+        <>
+           <button onClick={decrement}> - </button>
+           <h2> {count}</h2>
+           <button onClick={increment}> + </button>
+           {/* <Button onClick={reset}>RESET</Button> */}
+        </>
+    )
+
+    }
+    export default Counter
